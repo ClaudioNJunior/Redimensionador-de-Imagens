7c29189a92e300287c9f65caa4254527e8242c96
Claudio Junior <claudio.jr@targetone.com.br>
2020-08-27 01:54:23 -0300

    Feito redimensionamento de arquivos
    Neste momento está apenas indo para uma pasta, precisa alterar para fazer o download de um zip com todos os arquivos
    
    Falta fazer todas as validações ainda

--- Redimensionador de Imagens/Controllers/HomeController.cs
+++ Redimensionador de Imagens/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +30,49 @@ namespace Redimensionador_de_Imagens.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Redimensionar([FromForm] ArquivosProporcaoModel model)
+        {
+            if (model == null)
+                throw new Exception("Sem objeto");
+
+            if (model.Arquivos.Count == 0 || model.Width <= 0 || model.Height <= 0)
+                throw new Exception("Todos os campos são essenciais");
+
+            foreach (var arquivo in model.Arquivos)
+            {
+                if (arquivo.Length > 0)
+                {
+                    //byte[] imagem;
+
+                    using (var ms = new MemoryStream())
+                    {
+                        arquivo.CopyTo(ms);
+                        //imagem = ms.ToArray();
+
+                        float width = model.Width;
+                        float height = model.Height;
+                        var brush = new SolidBrush(Color.Black);
+                        var image = new Bitmap(Image.FromStream(ms));
+                        float scale = Math.Min(width / image.Width, height / image.Height);
+                        var bmp = new Bitmap((int)(image.Width * scale), (int)(image.Height * scale));
+                        var graph = Graphics.FromImage(bmp);
+                        graph.InterpolationMode = InterpolationMode.High;
+                        graph.CompositingQuality = CompositingQuality.HighQuality;
+                        graph.SmoothingMode = SmoothingMode.AntiAlias;
+                        var scaleWidth = (int)(image.Width * scale);
+                        var scaleHeight = (int)(image.Height * scale);
+                        graph.FillRectangle(brush, new RectangleF(0, 0, width, height));
+                        graph.DrawImage(image, 0, 0, scaleWidth, scaleHeight);
+
+                        bmp.Save("Importacoes/"+DateTime.Now.ToString("yyMMddHHmmssfff")+arquivo.FileName, ImageFormat.Jpeg);
+                    }
+                }
+            }
+
+            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Redimensionador de Imagens/Models/ArquivosProporcaoModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.Web;
+
+namespace Redimensionador_de_Imagens.Models
+{
+    public class ArquivosProporcaoModel
+    {
+        public List<IFormFile> Arquivos { get; set; } = new List<IFormFile>();
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
