0ed154dea3a79ec2c348b64b4eab2832b977a343
Claudio Junior <claudio.jr@targetone.com.br>
2020-08-27 00:32:32 -0300

    Criado Projeto

---
+++ Redimensionador de Imagens/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Redimensionador_de_Imagens.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
