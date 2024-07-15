0879932ac78b865d12ea3a3c3c02b125f38266e5
Claudio Junior <claudio.jr@targetone.com.br>
2024-07-15 11:10:13 -0300

    Executado projeto

--- Redimensionador de Imagens/obj/Debug/netcoreapp3.1/Redimensionador de Imagens.RazorTargetAssemblyInfo.cs
+++ Redimensionador de Imagens/obj/Debug/netcoreapp3.1/Redimensionador de Imagens.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Redimensionador de Imagens")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb3d2df4c1e246b400f7b417b3655cdf4570569")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Redimensionador de Imagens.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
